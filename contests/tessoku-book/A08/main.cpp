ca942b6eed3e89d949389fcadca169647c4c5d8f
Ryo Kawamura <kawa1214mura@gmail.com>
2022-11-05 17:40:44 +0900

    B08

--- contests/tessoku-book/A08/main.cpp
+++ contests/tessoku-book/A08/main.cpp
@@ -25,6 +25,55 @@ using namespace std;
 
 int main()
 {
+    int H, W;
+    cin >> H >> W;
+
+    vector<vector<int>> X(1509, vector<int>(1509, 0));
+
+    for (int h = 1; h <= H; h++)
+    {
+        for (int w = 1; w <= W; w++)
+        {
+            cin >> X[h][w];
+        }
+    }
+
+    vector<vector<int>> sum(1509, vector<int>(1509, 0));
+
+    for (int h = 1; h <= H; h++)
+    {
+        sum[h][1] = X[h][1];
+        for (int w = 2; w <= W; w++)
+        {
+            sum[h][w] = sum[h][w - 1] + X[h][w];
+        }
+    }
+
+    for (int w = 1; w <= W; w++)
+    {
+        for (int h = 2; h <= H; h++)
+        {
+            sum[h][w] += sum[h - 1][w];
+        }
+    }
+    int Q;
+    cin >> Q;
+
+    // 左上(A[q], B[q]), 右下(C[q], D[q])
+    vector<int> A(100009, 0), B(100009, 0), C(100009, 0), D(100009, 0);
+
+    rep(i, Q) cin >> A[i] >> B[i] >> C[i] >> D[i];
+
+    rep(q, Q)
+    {
+        int ans = 0;
+        ans += sum[C[q]][D[q]];
+        ans += sum[A[q] - 1][B[q] - 1];
+        ans -= sum[A[q] - 1][D[q]];
+        ans -= sum[C[q]][B[q] - 1];
+
+        cout << ans << endl;
+    }
 
     return 0;
 }
